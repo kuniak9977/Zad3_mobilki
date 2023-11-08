4a9d3df507b2854a74c0963572836a061da7d423
kuniak9977 <kunio9977@wp.pl>
2023-11-08 17:40:33 +0100

    Initial commit

---
+++ app/src/main/java/com/modzel/todoapp/TaskListActivity.java
@@ -0,0 +1,11 @@
+package com.modzel.todoapp;
+
+import androidx.fragment.app.Fragment;
+
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
